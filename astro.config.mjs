af30da3fcbac820aa48aed6bebcab1ada5a0ff2a
Sridhar <2061018+sridharkrishna@users.noreply.github.com>
2022-12-29 02:56:30 +0000

    add repository info

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,13 @@ import Atomico from '@atomico/astro';
 
 // https://astro.build/config
 export default defineConfig({
-  vite: {
-    optimizeDeps: {},
-    ssr: {
-      external: ['atomico/html'],
+    site: 'https://aifuse.github.io',
+    base: '/aifuse-web',
+    vite: {
+        optimizeDeps: {},
+        ssr: {
+            external: ['atomico/html'],
+        },
     },
-  },
-  integrations: [Atomico({ cssLiterals: { minify: true } })],
+    integrations: [Atomico({ cssLiterals: { minify: true } })],
 });
