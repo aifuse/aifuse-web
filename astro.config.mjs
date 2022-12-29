d82bb6580818bf4efac1ea42239aa46ccea4404c
Sridhar <2061018+sridharkrishna@users.noreply.github.com>
2022-12-29 02:41:33 +0000

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import Atomico from '@atomico/astro';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+    optimizeDeps: {},
+    ssr: {
+      external: ['atomico/html'],
+    },
+  },
+  integrations: [Atomico({ cssLiterals: { minify: true } })],
+});
