5eab60ea593a0f0cf8c41316d1d68c3d1b9ae44c
Sridhar <2061018+sridharkrishna@users.noreply.github.com>
2023-01-02 14:51:46 +0000

    netlify cms integration partial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,92 @@
-import { defineConfig } from 'astro/config';
-import Atomico from '@atomico/astro';
+import { defineConfig } from "astro/config";
+import Atomico from "@atomico/astro";
+import NetlifyCMS from "astro-netlify-cms";
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://aifuse.github.io',
-    base: '/aifuse-web',
-    vite: {
-        optimizeDeps: {},
-        ssr: {
-            external: ['atomico/html'],
-        },
+  site: "https://aifuse.github.io",
+  base: "/aifuse-web",
+  experimental: {
+    integrations: true,
+  },
+  vite: {
+    optimizeDeps: {},
+    ssr: {
+      external: ["atomico/html"],
     },
-    integrations: [Atomico({ cssLiterals: { minify: true } })],
+  },
+  integrations: [
+    Atomico({ cssLiterals: { minify: true } }),
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: "git-gateway",
+          branch: "main",
+        },
+        publish_mode: "editorial_workflow",
+        media_folder: "public/uploads",
+        public_folder: "public/uploads",
+        site_url: "https://aifuse.com",
+        display_url: "https://aifuse.com",
+        logo_url: "https://aifuse.com/logo.svg",
+        collections: [
+          // Content collections
+          {
+            name: "posts",
+            label: "Posts",
+            folder: "src/pages/blog",
+            create: true,
+            slug: "{{slug}}",
+            label_singular: "Post",
+            fields: [
+              { label: "Title", name: "title", widget: "string" },
+              { label: "Description", name: "description", widget: "string" },
+              { label: "Date", name: "date", widget: "datetime" },
+              {
+                label: "Language",
+                name: "lang",
+                widget: "select",
+                options: ["en", "ta-IN"],
+                default: "en",
+              },
+              {
+                label: "Cover",
+                name: "cover",
+                widget: "image",
+                required: false,
+              },
+              { label: "Author", name: "author", widget: "string" },
+              {
+                label: "Layout",
+                name: "layout",
+                widget: "hidden",
+                default: "../../components/templates/BlogPostTemplate.astro",
+              },
+              {
+                label: "Tags",
+                name: "tags",
+                widget: "list",
+                allow_add: true,
+                default: ["IT"],
+              },
+              {
+                label: "Categories",
+                name: "categories",
+                widget: "list",
+                allow_add: true,
+                default: ["IT"],
+              },
+              {
+                label: "Draft",
+                name: "draft",
+                widget: "boolean",
+                default: false,
+              },
+              { label: "Body", name: "body", widget: "markdown" },
+            ],
+          },
+        ],
+      },
+    }),
+  ],
 });
